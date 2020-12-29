92958d800717f9b70f75e0475afbb31096f4b60d
Benjamin-06 <75494783+Benjamin-06@users.noreply.github.com>
2020-12-29 17:49:59 +0100

    Building1

---
+++ Assets/Build.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Copyright 2020 7-Script
+public class Build : MonoBehaviour
+{
+
+    private GameObject selectedObject;
+
+    public void BuildTrack(GameObject partToBuild)
+    {
+        selectedObject = partToBuild;
+    }
+}
